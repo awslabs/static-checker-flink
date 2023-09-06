36ebb68611e8e59c02bc9a308c67ffafb2ff5764
Elphas Toringepi <etoringe@amazon.com>
2023-09-06 20:06:38 +0100

    Kinesis connector dependencies for all Flink versions

--- src/main/java/com/amazonaws/kda/flinkchecker/FlinkKinesisVersionCheck.java
+++ src/main/java/com/amazonaws/kda/flinkchecker/FlinkKinesisVersionCheck.java
@@ -2,19 +2,24 @@ package com.amazonaws.kda.flinkchecker;
 
 import org.apache.maven.model.Dependency;
 
+import java.util.Optional;
+
 /**
  * Ensures that the version of the Kinesis connector `flink-connector-kinesis` includes critical fixes
- * required for production readiness.
+ * required for production readiness.<br/>
+ * <br/>
  * For Flink 1.15: flink-connector-kinesis `1.15.4` is recommended. A warning is issued if this is not the case.
- * flink-connector-kinesis `1.15.4` includes a fix for a critical bug where Flink Kinesis EFO Consumer can fail to stop gracefully (FLINK-31183)
- * Other Flink versions are not yet covered.
+ * flink-connector-kinesis `1.15.4` includes a fix for a critical bug where Flink Kinesis EFO Consumer can fail to stop
+ * gracefully (FLINK-31183)<br/>
+ * <br/>
+ * See {@link FlinkVersionDependencies} for the recommended Kinesis connectors matrix for all Flink versions
  */
 public class FlinkKinesisVersionCheck extends Check {
 
 	@Override
 	public CheckResult check(CheckParams params) {
 		Dependency flinkDep = params.dependencies.stream().filter(dep -> dep.getGroupId().equals("org.apache.flink") &&
-				dep.getArtifactId().equals("flink-streaming-java")).findAny().orElse(null);
+				dep.getArtifactId().startsWith("flink-streaming-java")).findAny().orElse(null);
 
 		CheckResult result = new CheckResult().checkName("Kinesis Connector Version Check").success(true);
 		if (flinkDep == null) {
@@ -24,42 +29,43 @@ public class FlinkKinesisVersionCheck extends Check {
 
 		String flinkVersion = flinkDep.getVersion();
 
-		Dependency kinesisDep = params.dependencies.stream().filter(dep -> dep.getGroupId().equals("org.apache.flink") &&
-				dep.getArtifactId().equals("flink-connector-kinesis")).findAny().orElse(null);
+		Optional<FlinkVersionDependencies> flinkVersionDeps = FlinkVersionDependencies.from(flinkVersion);
+
+		if (!flinkVersionDeps.isPresent()) {
+			return result;
+		}
+
+		Dependency requiredDep = flinkVersionDeps.get().getDependency();
+
+		Dependency kinesisDep = params.dependencies.stream()
+				.filter(dep -> {
+					String artifactId = dep.getArtifactId();
+					return artifactId.startsWith(FlinkVersionDependencies.KINESIS_CONNECTOR_ARTIFACT_ID)
+							|| artifactId.startsWith(FlinkVersionDependencies.KINESIS_EFO_CONNECTOR_ARTIFACT_ID);
+				})
+				.findAny()
+				.orElse(null);
 
 		if (kinesisDep == null) {
 			params.log.debug("No Flink kinesis connector dependency found");
 			return result;
 		}
 
-		if (flinkVersion.startsWith("1.15")) {
-			String requiredKinesisConnectorVersion = "1.15.4";
-			if (kinesisDep.getVersion().equals(requiredKinesisConnectorVersion)) {
-				params.log.info("✅ Flink Kinesis connector version check passed.");
-				return result;
-			}
-
-			params.log.warn(String.format("❌ For Flink %s, the recommended version for org.apache.flink:flink-connector-kinesis is %s or later",
-					flinkVersion, requiredKinesisConnectorVersion));
+		String requiredVersion = requiredDep.getVersion();
+		if (!kinesisDep.getGroupId().equals(requiredDep.getGroupId())
+				|| !kinesisDep.getArtifactId().equals(requiredDep.getArtifactId())) {
+			params.log.warn(String.format("❌ For Flink %s, the recommended Kinesis connector dependency is %s:%s:%s",
+					flinkVersion, requiredDep.getGroupId(), requiredDep.getArtifactId(), requiredVersion));
 			return result.success(false);
 		}
 
-		if (flinkVersion.startsWith("1.13")) {
-			return result;
+		if (!kinesisDep.getVersion().equals(requiredVersion)) {
+			params.log.warn(String.format("❌ For Flink %s, the recommended version for %s:%s is %s or later",
+					flinkVersion, requiredDep.getGroupId(), requiredDep.getArtifactId(), requiredVersion));
+			return result.success(false);
 		}
 
-		if (flinkVersion.startsWith("1.11")) {
-			return result;
-		}
-
-		if (flinkVersion.startsWith("1.8")) {
-			return result;
-		}
-
-		if (flinkVersion.startsWith("1.6")) {
-			return result;
-		}
-
+		params.log.info("✅ Flink Kinesis connector version check passed.");
 		return result;
 	}
 }
